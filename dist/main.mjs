674c54c012a06b55fd130fa414aa8210988d9aec
Mitch <238069586+mitcht-dev@users.noreply.github.com>
2026-06-17 16:08:32 -0700

    push some basic stuff to see if anything works

---
+++ dist/main.mjs
@@ -0,0 +1,96 @@
+const clientId = '4908b012-a9f6-45e2-92bd-4171de2fbef7';
+const redirectUri = 'http://localhost:8080/index.html';
+
+const appName = 'transcriptApp';
+const qParamLanguage = 'language';
+const qParamEnvironment = 'environment';
+
+const qParamConversationId = 'gcConversationId';
+
+// Default values are assigned but values should 
+// be set on the function 'assignConfiguration'
+let language = 'en-us';
+let environment = 'mypurecloud.com';
+
+let userDetails = null;
+let conversationId = null;
+
+/**
+ * Configure both the Platform SDK and the Client App SDK
+ */
+function setupGenesysClients() {
+  const platformClient = require('platformClient');
+  const client = platformClient.ApiClient.instance;
+  const usersApi = new platformClient.UsersApi();
+
+  // Configure Client App
+  let ClientApp = window.purecloud.apps.ClientApp;
+  let transcriptApp = new ClientApp({
+    pcEnvironment: environment
+  });
+
+
+  // Configure and Authenticate Platform Client
+  client.setPersistSettings(true, appName);
+  client.setEnvironment(environment);
+
+  return client.loginImplicitGrant(clientId, redirectUri)
+    .then(data => usersApi.getUsersMe())
+    .then(data => {
+      userDetails = data;
+
+      transcriptApp.alerting.showToastPopup(
+        `Hi ${userDetails.name}`,
+        'Never gonna give you up, never gonna let you down 😊');
+    })
+    .catch(err => console.log(err));
+
+}
+
+/**
+ * Assign the language and environment for the app first through
+ * the query parameters. But if non-existent, attempt to get
+ * it from localStorage. If none, use default values.
+ */
+function assignConfiguration() {
+  let url = new URL(window.location);
+  let searchParams = new URLSearchParams(url.search);
+
+  if (searchParams.has(qParamLanguage)) {
+    language = searchParams.get(qParamLanguage);
+    localStorage.setItem(`${appName}_language`, language);
+  } else {
+    let local_lang = localStorage.getItem(`${appName}_language`);
+    if (local_lang) language = local_lang;
+  }
+
+  if (searchParams.has(qParamEnvironment)) {
+    environment = searchParams.get(qParamEnvironment);
+    localStorage.setItem(`${appName}_environment`, environment);
+  } else {
+    let local_env = localStorage.getItem(`${appName}_environment`);
+    if (local_env) environment = local_env;
+  }
+
+  if (searchParams.has(qParamConversationId)) {
+    conversationId = searchParams.get(qParamConversationId)
+  }
+}
+
+
+// After page loads...
+window.addEventListener('load', (event) => {
+  assignConfiguration();
+  console.log(`environment: ${environment}`);
+  console.log(`language: ${language}`);
+
+  setupGenesysClients()
+    .then(() => {
+      // Display values to the page
+      document.getElementById('span_environment').innerText = environment;
+      document.getElementById('span_language').innerText = language;
+      document.getElementById('span_name').innerText = userDetails.name;
+
+      console.log('Finished setup.');
+    })
+});
