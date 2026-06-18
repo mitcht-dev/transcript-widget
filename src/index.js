884e7abfff05e3dc0a8fb1aa1a2ab79f3db37a57
Mitch <238069586+mitcht-dev@users.noreply.github.com>
2026-06-18 12:56:07 -0700

    fixes

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ const redirectUri = 'https://mitcht-dev.github.io/transcription-widget/';
 
 const appName = 'Transcript App';
 
-  const urlParams = new URLSearchParams(window.location.search);
-  let targetEnv = urlParams.get('gcTargetEnv');
+const urlParams = new URLSearchParams(window.location.search);
+let targetEnv = urlParams.get('gcTargetEnv');
 
-  if (targetEnv) {
-    sessionStorage.setItem('gc_environment', targetEnv);
-  } else {
-    targetEnv = sessionStorage.getItem('gc_environment') || 'usw2.pure.cloud';
-  }
+if (targetEnv) {
+  sessionStorage.setItem('gc_environment', targetEnv);
+} else {
+  targetEnv = sessionStorage.getItem('gc_environment') || 'usw2.pure.cloud';
+}
 
-  environment = targetEnv;
+let environment = targetEnv;
 
 /**
  * Configure both the Platform SDK and the Client App SDK
@@ -30,14 +30,12 @@ function setupGenesysClients() {
     gcTargetEnvQueryParam: 'gcTargetEnv'
   });
 
-  let environment = transcriptApp.gcEnvironment;
-
   // Configure and Authenticate Platform Client
   client.setPersistSettings(true, appName);
   client.setEnvironment(environment);
 
   return client.loginPKCEGrant(clientId, redirectUri)
-    .then(data => 
+    .then(data =>
       console.log('Success message!')
     )
     .catch(err => console.error('Authentication Failed:', err));
@@ -45,7 +43,7 @@ function setupGenesysClients() {
 
 function initializeWidget() {
   console.log("Widget script is running...");
-  
+
   console.log(`environment: ${environment}`);
 
   setupGenesysClients()
@@ -55,7 +53,7 @@ function initializeWidget() {
 }
 
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initializeWidget);
+  document.addEventListener('DOMContentLoaded', initializeWidget);
 } else {
-    initializeWidget();
+  initializeWidget();
 }
